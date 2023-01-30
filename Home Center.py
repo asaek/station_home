dc80de15e830261c9e8cf844ec56e882b8a655b8
Asaek <asaek60@gmail.com>
2023-01-30 09:59:31 -0600

    agregada ip y boton

--- Home Center.py
+++ Home Center.py
@@ -3,7 +3,14 @@ from threading import Thread
 from tm1637 import TM1637
 import Adafruit_DHT
 import drivers
-from datetime import datetime
+import RPi.GPIO as GPIO
+
+# import socket
+# import fcntl
+# import struct
+import os
+import subprocess
+
 
 DIOHora = 27
 CLKHora = 22
@@ -12,14 +19,28 @@ DIOCronometro = 17
 CLKCronometro = 4
 
 display = drivers.Lcd()
+pin_btn = 21
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+ip = subprocess.check_output(['hostname', '-I']).decode().strip()
+
+# def get_ip_address(ifname):
+#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     return socket.inet_ntoa(fcntl.ioctl(
+#         s.fileno(),
+#         0x8915,
+#         struct.pack('256s', ifname[:15])
+#     )[20:24])
+
 
 class Clock:
     def __init__(self, tm_instance):
         self.tm = tm_instance
         self.show_colon = False
-        
-    def run(self):
 
+    def run(self):
         while True:
             t = localtime()
             if t.tm_hour > 12:
@@ -28,24 +49,6 @@ class Clock:
                 hora = t.tm_hour
             self.show_colon = not self.show_colon
             tm.numbers(hora, t.tm_min, self.show_colon)
-#             if GPIO.input(32) == GPIO.HIGH:
-#                 print("Button was pushed!")
-
-            horaJapon = 0
-            diaSiguiente = False
-            tiempoHora = t.tm_hour
-            tiempoMin = t.tm_min
-            horaJapon = tiempoHora + 15
-
-            if horaJapon > 24:
-                horaJapon = horaJapon - 24
-                diaSiguiente = True
-            
-            if diaSiguiente == True:
-                display.lcd_display_string('Tokyo: ' + str(horaJapon) + ':' + str(tiempoMin) + ' |*|', 1)
-            else:
-                display.lcd_display_string('Tokyo: ' + str(horaJapon) + ':' + str(tiempoMin) , 1)
-                
             sleep(1)
 
 
@@ -69,18 +72,60 @@ class Temp:
                 #print("Failed to retrieve data from humidity sensor")
 
 
+class Lcd20x4:
+    def __init__(self):
+        self.running = True
+
+        # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # return socket.inet_ntoa(fcntl.ioctl(
+        #     s.fileno(),
+        #     0x8915,
+        #     struct.pack('256s', ifname[:15])
+        # )[20:24])
+
+    def run(self):
+        horaJapon = 0
+        diaSiguiente = False
+
+        while True:
+            if GPIO.input(pin_btn) == GPIO.HIGH:
+                print('Me estas tocando bb')
+                # display.lcd_display_extended_string('Luz Encendida', 4)
+            # else:
+                # display.lcd_display_extended_string('Luz Apagada     ', 4)
+            display.lcd_display_string(ip, 4)
+
+            t = localtime()
+            tiempoHora = t.tm_hour
+            tiempoMin = t.tm_min
+            horaJapon = tiempoHora + 15
+            if horaJapon > 24:
+                horaJapon = horaJapon - 24
+                diaSiguiente = True
+            if diaSiguiente == True:
+                display.lcd_display_string(
+                    'Tokyo: ' + str(horaJapon) + ':' + str(tiempoMin) + ' |*|', 1)
+
+            else:
+                display.lcd_display_string(
+                    'Tokyo: ' + str(horaJapon) + ':' + str(tiempoMin) + '      ', 1)
+
+
 if __name__ == '__main__':
+    display.lcd_backlight(0)
     tm = TM1637(CLKHora, DIOHora)
     tm.brightness(7)
     Reloj = Clock(tm)
+
     RelojThread = Thread(target=Reloj.run)
     RelojThread.start()
 
     tm2 = TM1637(CLKCronometro, DIOCronometro)
     tm2.brightness(7)
-#     CronometroRun = Cronometro(tm2)
-#     CronometroThread = Thread(target=CronometroRun.run)
-#     CronometroThread.start()
+
+    PantallaAzul = Lcd20x4()
+    PantallaAzulThread = Thread(target=PantallaAzul.run)
+    PantallaAzulThread.start()
 
     Temperatura = Temp(tm2)
     TemperaturaThread = Thread(target=Temperatura.run)
